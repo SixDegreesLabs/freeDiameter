dd12977be2a8417f55c84f9580180e8c79983e0f
Adrian Plunkett <adrian.plunkett@sixd.co>
2023-03-09 17:01:09 +0300

    misc updates to support deployment at lebara

--- extensions/test_cc/test_cc.c
+++ extensions/test_cc/test_cc.c
@@ -32,7 +32,8 @@
 /* This extension simply receives CCR and sends CCA after displaying the content, but does not store any data */
 
 #include <freeDiameter/extension.h>
-#include <sqlite3.h>
+#include <nats/nats.h>
+
 
 
 struct disp_hdl *ccr_handler_hdl;
@@ -40,72 +41,75 @@ struct disp_hdl *ccr_handler_hdl;
 struct dict_object * aai_avp_do; /* cache the Auth-Application-Id dictionary object */
 struct dict_object * crn_avp_do; /* cache the CC-Request-Number dictionary object */
 struct dict_object * crt_avp_do; /* cache the CC-Request-Type dictionary object */
+struct dict_object * si_avp_do; /* cache the Subscription-Id dictionary object */
+struct dict_object * sinfo_avp_do; /* cache the Service-Information dictionary object */
 
 #define MODULE_NAME "test_cc"
 
 struct statistics {
-        uint64_t sent;
-        time_t first;
-        time_t last;
+	uint64_t sent;
+	time_t first;
+	time_t last;
 } statistics;
 
-sqlite3 *db;
+natsConnection      *conn = NULL;
+natsMsg             *reply= NULL;
 
 /**
  * Check if an MSISDN is in the blacklist.
  * Returns true or false
- */
-_Bool lookup_blacklist(sqlite3* db, char *msisdn)
-{
-    sqlite3_stmt *stmt;
-    int rc = sqlite3_prepare_v2(db, "SELECT count(*)"
-                                    " FROM blacklist"
-                                    " WHERE msisdn = ?", -1, &stmt, NULL);
-    if (rc != SQLITE_OK) {
-        fd_log_error("preparing database query: %s", sqlite3_errmsg(db));
-	    return 0;
-    }
+ _Bool lookup_blacklist(sqlite3* db, char *msisdn)
+ {
+ sqlite3_stmt *stmt;
+ int rc = sqlite3_prepare_v2(db, "SELECT count(*)"
+ " FROM blacklist"
+ " WHERE msisdn = ?", -1, &stmt, NULL);
+ if (rc != SQLITE_OK) {
+ fd_log_error("preparing database query: %s", sqlite3_errmsg(db));
+ return 0;
+ }
 
-    rc = sqlite3_bind_text(stmt, 1, msisdn, strlen(msisdn), NULL);
-    if (rc != SQLITE_OK) {                 
-        fd_log_error("binding database query: %s", sqlite3_errmsg(db));
-        sqlite3_finalize(stmt);            
-        return 0;                      
-    }
+ rc = sqlite3_bind_text(stmt, 1, msisdn, strlen(msisdn), NULL);
+ if (rc != SQLITE_OK) {                 
+ fd_log_error("binding database query: %s", sqlite3_errmsg(db));
+ sqlite3_finalize(stmt);            
+ return 0;                      
+ }
 
-    rc = sqlite3_step(stmt);
-    if (rc != SQLITE_ROW && rc != SQLITE_DONE) {
-        fd_log_error("binding database query: %s", sqlite3_errmsg(db));
-        sqlite3_finalize(stmt);            
-        return 0;                      
-    }
-    /*if (rc == SQLITE_DONE) {
-        sqlite3_finalize(stmt);
-        throw string("customer not found");
-    }*/
+ rc = sqlite3_step(stmt);
+ if (rc != SQLITE_ROW && rc != SQLITE_DONE) {
+ fd_log_error("binding database query: %s", sqlite3_errmsg(db));
+ sqlite3_finalize(stmt);            
+ return 0;                      
+ }
+//if (rc == SQLITE_DONE) {
+//    sqlite3_finalize(stmt);
+//    throw string("customer not found");
+// }
 
-    _Bool blacklisted = sqlite3_column_int(stmt, 0);
+_Bool blacklisted = sqlite3_column_int(stmt, 0);
 
-    sqlite3_finalize(stmt);
+sqlite3_finalize(stmt);
 
-    return blacklisted;
+return blacklisted;
 }
+*/
 
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-	int i;
-	for(i=0; i<argc; i++){
-		fd_log_error("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-	}
-	return 0;
-}
+//static int callback(void *NotUsed, int argc, char **argv, char **azColName){
+//	int i;
+//	for(i=0; i<argc; i++){
+//		fd_log_error("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+//	}
+//	return 0;
+//}
 
 void print_statistics(void) {
-        if (statistics.first == 0 || statistics.last == 0 || statistics.last == statistics.first) {
-                return;
-        }
+	if (statistics.first == 0 || statistics.last == 0 || statistics.last == statistics.first) {
+		return;
+	}
 
-        fd_log_error("%s: %lld CCA messages sent in %llds (%.2f messages/second)", fd_g_config->cnf_diamid,
-                     (long long)statistics.sent, (long long)(statistics.last-statistics.first), (float)statistics.sent / (statistics.last-statistics.first));
+	fd_log_error("%s: %lld CCA messages sent in %llds (%.2f messages/second)", fd_g_config->cnf_diamid,
+			(long long)statistics.sent, (long long)(statistics.last-statistics.first), (float)statistics.sent / (statistics.last-statistics.first));
 }
 
 static int ccr_handler(struct msg ** msg, struct avp * avp, struct session * sess, void * data, enum disp_action * act)
@@ -129,7 +133,10 @@ static int ccr_handler(struct msg ** msg, struct avp * avp, struct session * ses
 		struct avp *avp_data;
 		struct avp_hdr *ahdr;
 		uint32_t crt, crn;
-    char *response_code = "SUCCESS";
+		uint8_t *sid = NULL;
+		uint8_t *callingPartyAddr = NULL;
+		uint8_t *calledPartyAddr = NULL;
+		char *response_code = "SUCCESS";
 
 		/* get some necessary information from request */
 		if (fd_msg_search_avp(*msg, crt_avp_do, &avp_data) < 0 || avp_data == NULL) {
@@ -141,6 +148,7 @@ static int ccr_handler(struct msg ** msg, struct avp * avp, struct session * ses
 			return 0;
 		}
 		crt = ahdr->avp_value->i32;
+
 		if (fd_msg_search_avp(*msg, crn_avp_do, &avp_data) < 0 || avp_data == NULL) {
 			fd_log_error("[%s] CC-Request-Number not found in CCR", MODULE_NAME);
 			return 0;
@@ -151,11 +159,78 @@ static int ccr_handler(struct msg ** msg, struct avp * avp, struct session * ses
 		}
 		crn = ahdr->avp_value->i32;
 
+		if (fd_msg_search_avp(*msg, si_avp_do, &avp_data) < 0 || avp_data == NULL) {
+			fd_log_error("[%s] Subscription-Id not found in CCR", MODULE_NAME);
+			return 0;
+		}
+
+		CHECK_FCT(  fd_msg_browse(avp_data, MSG_BRW_FIRST_CHILD, &avp, NULL)  );
+		while (avp) {
+			CHECK_FCT(  fd_msg_avp_hdr( avp, &ahdr )  );
+			switch (ahdr->avp_code) {
+				case 444: // Subscription-Id-Data
+					CHECK_FCT( fd_msg_parse_dict ( avp, fd_g_config->cnf_dict, NULL ) );
+					ASSERT( ahdr->avp_value );
+					sid = ahdr->avp_value->os.data;
+					TRACE_DEBUG(INFO, "Received an AVP Subscription-Id-Data  %d ('%.*s')", ahdr->avp_code, (int)ahdr->avp_value->os.len, ahdr->avp_value->os.data);
+					break;
+				case 450: // Subscription-Id-Type
+					CHECK_FCT( fd_msg_parse_dict ( avp, fd_g_config->cnf_dict, NULL ) );
+					ASSERT( ahdr->avp_value );
+					TRACE_DEBUG(INFO, "Received an AVP Subscription-Id-Type %d ('%d')", ahdr->avp_code, ahdr->avp_value->i32);
+					break;
+				default:
+					TRACE_DEBUG(INFO, "Received an invalid AVP value %d ('%.*s'), ignored", ahdr->avp_code, (int)ahdr->avp_value->os.len, ahdr->avp_value->os.data);
+			}
+			/* Go to next AVP */
+			CHECK_FCT(  fd_msg_browse(avp, MSG_BRW_NEXT, &avp, NULL)  );
+		}
+
+
+		if (fd_msg_search_avp(*msg, sinfo_avp_do, &avp_data) < 0 || avp_data == NULL) {
+			fd_log_error("[%s] Service-Information not found in CCR", MODULE_NAME);
+			return 0;
+		}
+
+		CHECK_FCT(  fd_msg_browse(avp_data, MSG_BRW_FIRST_CHILD, &avp, NULL)  );
+		while (avp) {
+			CHECK_FCT(  fd_msg_avp_hdr( avp, &ahdr )  );
+			switch (ahdr->avp_code) {
+				case 20300: // IN-Information
+					TRACE_DEBUG(INFO, "Received an AVP IN-Information  %d", ahdr->avp_code);
+
+					struct avp *havp;
+					CHECK_FCT(  fd_msg_browse(avp, MSG_BRW_FIRST_CHILD, &havp, NULL)  );
+					while (havp) {
+						CHECK_FCT(  fd_msg_avp_hdr( havp, &ahdr )  );
+						if (ahdr->avp_code == 20336) { // Calling-Party-Address
+							CHECK_FCT( fd_msg_parse_dict ( havp, fd_g_config->cnf_dict, NULL ) );
+							ASSERT( ahdr->avp_value );
+							callingPartyAddr = ahdr->avp_value->os.data;
+							TRACE_DEBUG(INFO, "Received an AVP Calling-Party-Address  %d ('%.*s')", ahdr->avp_code, (int)ahdr->avp_value->os.len, ahdr->avp_value->os.data);
+						} else if (ahdr->avp_code == 20337) { // Called-Party-Address
+							CHECK_FCT( fd_msg_parse_dict ( havp, fd_g_config->cnf_dict, NULL ) );
+							ASSERT( ahdr->avp_value );
+							calledPartyAddr = ahdr->avp_value->os.data;
+							TRACE_DEBUG(INFO, "Received an AVP Called-Party-Address  %d ('%.*s')", ahdr->avp_code, (int)ahdr->avp_value->os.len, ahdr->avp_value->os.data);
+						} else {
+							TRACE_DEBUG(INFO, "Received an invalid AVP value  %d", ahdr->avp_code);
+						}
+						/* Go to next AVP */
+						CHECK_FCT(  fd_msg_browse(havp, MSG_BRW_NEXT, &havp, NULL)  );
+					}
+					break;
+				default:
+					TRACE_DEBUG(INFO, "Received an invalid AVP value %d ('%.*s'), ignored", ahdr->avp_code, (int)ahdr->avp_value->os.len, ahdr->avp_value->os.data);
+			}
+			/* Go to next AVP */
+			CHECK_FCT(  fd_msg_browse(avp, MSG_BRW_NEXT, &avp, NULL)  );
+		}
+
+
 		/* Create the answer message */
 		CHECK_FCT(fd_msg_new_answer_from_req(fd_g_config->cnf_dict, msg, 0));
 		answer = *msg;
-
-		/* TODO copy/fill in more AVPs in the answer */
 
 		/* Auth-Application-Id */
 		fd_msg_avp_new(aai_avp_do, 0, &avp);
@@ -190,25 +265,51 @@ static int ccr_handler(struct msg ** msg, struct avp * avp, struct session * ses
 		}
 		fd_msg_avp_add(answer, MSG_BRW_LAST_CHILD, avp);
 
-		/* TODO make result configurable (depending on an AVP?) */
-		//CHECK_FCT(fd_msg_rescode_set(answer, "DIAMETER_SUCCESS", NULL, NULL, 1));
-
-		char *msisdn = "447956432990";
-		_Bool blacklisted = lookup_blacklist(db, msisdn);
-		fd_log(FD_LOG_INFO, "msisdn %s is blacklisted? %s", msisdn, blacklisted);
-		if (blacklisted) {
-			response_code = "TRANSIENT_FAILURE";
-		} else {
-				response_code = "SUCCESS";
-		}
-		CHECK_FCT(fd_msg_rescode_set(answer, response_code, NULL, NULL, 1));
 
 		fd_log_debug("--------------Received the following Credit Control Request:--------------");
 
 		CHECK_FCT(fd_sess_getsid(sess, &s, &sl));
 		fd_log_debug("Session: %.*s",(int)sl, s);
 
-		fd_log_debug("----------------------------------------------------------------------");
+		// send String to nats anum,bnum
+		if (callingPartyAddr == NULL) {
+			callingPartyAddr = sid;
+		} else if (calledPartyAddr == NULL) {
+			calledPartyAddr = sid;
+		}
+		fd_log_debug("--------------Sending request to NATS:--------------");
+		char req[80];
+		sprintf(req, "%s,%s",callingPartyAddr, calledPartyAddr);
+		natsStatus ns = natsConnection_RequestString(&reply, conn, "diameter", req, 100);
+		if (ns == NATS_OK)
+		{
+			// If we are here, we should have received the reply
+			printf("Received reply: %.*s\n",
+					natsMsg_GetDataLength(reply),
+					natsMsg_GetData(reply));
+        
+			// Need to destroy the message!
+			natsMsg_Destroy(reply);
+		} else {
+		  fd_log_error("nats status %d", ns);
+		}
+
+		if (strcmp((const char *)callingPartyAddr, "96561084769") == 0) {
+			fd_log(FD_LOG_INFO, "msisdn %s calling %s is Wangiri", callingPartyAddr, calledPartyAddr );
+			response_code = "PLAY_ANNOUNCEMENT";
+
+		} else if (strcmp((const char *)calledPartyAddr, "966579183132") == 0) {
+			fd_log(FD_LOG_INFO, "msisdn %s calling %s is blacklisted", callingPartyAddr, calledPartyAddr );
+			response_code = "TRANSIENT_FAILURE";
+
+		} else {
+			fd_log(FD_LOG_INFO, "msisdn %s calling %s is successful", callingPartyAddr, calledPartyAddr );
+			response_code = "SUCCESS";
+		}
+
+
+		CHECK_FCT(fd_msg_rescode_set(answer, response_code, NULL, NULL, 1));
+
 
 		/* Send the answer */
 		CHECK_FCT(fd_msg_send(msg, NULL, NULL));
@@ -234,43 +335,46 @@ static int ccr_handler(struct msg ** msg, struct avp * avp, struct session * ses
 /* entry hook: register callback */
 static int cc_entry(char * conffile)
 {
+	// Creates a connection to the default NATS URL
+	natsStatus s = natsConnection_ConnectTo(&conn, NATS_DEFAULT_URL);
+	// If there was an error, print a stack trace and exit
+	if (s != NATS_OK)
+	{
+		nats_PrintLastErrorStack(stderr);
+		fd_log_error("Can't open NATS: %s\n", NATS_DEFAULT_URL);
+		return(1);
+	}
+
 	struct disp_when data;
 
 	TRACE_ENTRY("%p", conffile);
 
 	CHECK_FCT_DO(fd_dict_search(fd_g_config->cnf_dict, DICT_AVP, AVP_BY_NAME, "Auth-Application-Id", &aai_avp_do, ENOENT),
-		     { LOG_E("Unable to find 'Auth-Application-Id' AVP in the loaded dictionaries."); });
+			{ LOG_E("Unable to find 'Auth-Application-Id' AVP in the loaded dictionaries."); });
 	CHECK_FCT_DO(fd_dict_search(fd_g_config->cnf_dict, DICT_AVP, AVP_BY_NAME, "CC-Request-Number", &crn_avp_do, ENOENT),
-		     { LOG_E("Unable to find 'CC-Request-Number' AVP in the loaded dictionaries."); });
+			{ LOG_E("Unable to find 'CC-Request-Number' AVP in the loaded dictionaries."); });
 	CHECK_FCT_DO(fd_dict_search(fd_g_config->cnf_dict, DICT_AVP, AVP_BY_NAME, "CC-Request-Type", &crt_avp_do, ENOENT),
-		     { LOG_E("Unable to find 'CC-Request-Type' AVP in the loaded dictionaries."); });
+			{ LOG_E("Unable to find 'CC-Request-Type' AVP in the loaded dictionaries."); });
+
+	/* latro extensions */
+	CHECK_FCT_DO(fd_dict_search(fd_g_config->cnf_dict, DICT_AVP, AVP_BY_NAME, "Subscription-Id", &si_avp_do, ENOENT),
+			{ LOG_E("Unable to find 'Subscription-Id' AVP in the loaded dictionaries."); });
+
+	struct dict_avp_request req;
+	req.avp_vendor = 10415;
+	req.avp_name = "Service-Information";
+	CHECK_FCT_DO(fd_dict_search(fd_g_config->cnf_dict, DICT_AVP, AVP_BY_NAME_AND_VENDOR, &req, &sinfo_avp_do, ENOENT),
+		     { LOG_E("Unable to find 'Service-Information' AVP in the loaded dictionaries."); });
 
 	memset(&data, 0, sizeof(data));
 
-        /* Advertise the support for the Diameter Credit Control application in the peer */
-        CHECK_FCT( fd_dict_search( fd_g_config->cnf_dict, DICT_APPLICATION, APPLICATION_BY_NAME, "Diameter Credit Control Application", &data.app, ENOENT) );
-        CHECK_FCT( fd_disp_app_support ( data.app, NULL, 1, 0 ) );
+	/* Advertise the support for the Diameter Credit Control application in the peer */
+	CHECK_FCT( fd_dict_search( fd_g_config->cnf_dict, DICT_APPLICATION, APPLICATION_BY_NAME, "Diameter Credit Control Application", &data.app, ENOENT) );
+	CHECK_FCT( fd_disp_app_support ( data.app, NULL, 1, 0 ) );
 
-        /* register handler for CCR */
-        CHECK_FCT( fd_dict_search( fd_g_config->cnf_dict, DICT_COMMAND, CMD_BY_NAME, "Credit-Control-Request", &data.command, ENOENT) );
-        CHECK_FCT( fd_disp_register( ccr_handler, DISP_HOW_CC, &data, NULL, &ccr_handler_hdl ) );
-
-	char *dbfile = "/tmp/rvs.db";
-	char *zErrMsg = 0;
-	int rc = sqlite3_open(dbfile, &db);
-	if( rc ){
-		fd_log_error("Can't open database: %s\n", sqlite3_errmsg(db));
-		sqlite3_close(db);
-		return(1);
-	}
-	fd_log_error("Database opened: %s\n", dbfile);
-
-	char *sqlstmt = "create table if not exists blacklist (msisdn String primary key, created timestamp default current_timestamp);";
-	rc = sqlite3_exec(db, sqlstmt, callback, 0, &zErrMsg);
-	if( rc!=SQLITE_OK ){
-		fd_log_error("SQL error: %s\n", zErrMsg);
-		sqlite3_free(zErrMsg);
-	}
+	/* register handler for CCR */
+	CHECK_FCT( fd_dict_search( fd_g_config->cnf_dict, DICT_COMMAND, CMD_BY_NAME, "Credit-Control-Request", &data.command, ENOENT) );
+	CHECK_FCT( fd_disp_register( ccr_handler, DISP_HOW_CC, &data, NULL, &ccr_handler_hdl ) );
 
 	return 0;
 }
@@ -278,16 +382,16 @@ static int cc_entry(char * conffile)
 /* And terminate it */
 void fd_ext_fini(void)
 {
-        /* Unregister the callbacks */
-        if (ccr_handler_hdl) {
-                CHECK_FCT_DO( fd_disp_unregister(&ccr_handler_hdl, NULL), );
-                ccr_handler_hdl = NULL;
-        }
+	/* Unregister the callbacks */
+	if (ccr_handler_hdl) {
+		CHECK_FCT_DO( fd_disp_unregister(&ccr_handler_hdl, NULL), );
+		ccr_handler_hdl = NULL;
+	}
 
 	print_statistics();
-	sqlite3_close(db);
 
-        return;
+	natsConnection_Destroy(conn);
+	return;
 }
 
 
